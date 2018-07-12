7c3f74f2021a6248bd38f7ce86e1c4a2744be456
info4shajeer@gmail.com <info4shajeer@gmail.com>
2018-07-12 20:00:33 +0530

    updated read me

--- ReadAppSignature/www/ReadAppSignature.js
+++ ReadAppSignature/www/ReadAppSignature.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+exports.getAppSignature = function (arg0, success, error) {
     exec(success, error, 'ReadAppSignature', 'getAppSignature', [arg0]);
 };
